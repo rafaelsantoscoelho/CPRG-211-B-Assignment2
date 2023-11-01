465bff04110b10cd26cb8607914865bcd4ca800f
rafaelsantoscoelho <rafael.santoscoelho@edu.sait.ca>
2023-11-01 09:30:42 -0600

    Added a constructor to the AirportManager class

--- Backend/AirportManager.cs
+++ Backend/AirportManager.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Traveless.Backend.Entities;
 
 namespace Traveless.Backend
 {
     internal class AirportManager
     {
+        // Fields
+
+        private List<Airport> _airports;
+        private const string _AIRPORTS_FILE = "../../../../Backend/Data/airports.csv";
+
+        // Properties
+
+        public IList<Airport> Airports
+        {
+            get { return this._airports.ToList(); }
+        }
+
+        // Methods
+
+        public AirportManager()
+        {
+            this._airports = new List<Airport>();
+        }
+
+        public Airport FindAirport(string code)
+        {
+            foreach (Airport airport in this._airports)
+            {
+                if (airport.Code == code)
+                {
+                    return airport;
+                }
+            }
+
+            return null;
+        }
+
+        public void LoadAirports()
+        {
+            string[] lines = File.ReadAllLines(_AIRPORTS_FILE);
+
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(',');
+                string code = parts[0];
+                string name = parts[1];
+
+                Airport airport = new Airport(code, name);
+                _airports.Add(airport);
+            }
+        }
     }
 }
