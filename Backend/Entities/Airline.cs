2c1bdfc646ec7f78506146e4317909bb7034d1b5
rafaelsantoscoelho <rafael.santoscoelho@edu.sait.ca>
2023-11-01 08:30:30 -0600

    Basic implementation for the Airline class

--- Backend/Entities/Airline.cs
+++ Backend/Entities/Airline.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,28 @@ namespace Traveless.Backend.Entities
 {
     internal class Airline
     {
+        // Fields
+
+        private string _code;
+        private string _name;
+
+        // Properties
+
+        public string Code
+        {
+            get { return _code; }
+            set { _code = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public override string ToString()
+        {
+            return "";
+        }
     }
 }
